60604c90f5240a96147d9d4674cd80fea8edafda
WilliamOtieno <jimmywilliamotieno@gmail.com>
2021-05-12 02:04:14 +0300

    added urls

--- core/urls.py
+++ core/urls.py
@@ -3,10 +3,13 @@ from django.conf.urls.static import static
 from django.urls import path, include
 from . import views
 
-app_name = "core"
 
 urlpatterns = [
     path("", views.login, name="login"),
     path("login", views.login, name="login"),
-    path("dashboard")
+    path("dashboard", views.dashboard, name="dashboard"),
+    path("create-appointment", views.create_appointment, name="create-appointment"),
+    path("today-appointment", views.today_appointment, name="today-appointment"),
+    path("all-appointment", views.all_appointment, name="all-appointment"),
+    path("register", views.register, name="register")
 ]
